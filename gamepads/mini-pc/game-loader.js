01fe9875330b6642c735da5ba2da5a4816e1ae68
oddalan <0ddalan300@gmail.com>
2025-08-24 17:53:59 -0600

    ?

---
+++ gamepads/mini-pc/game-loader.js
@@ -0,0 +1,4 @@
+function loadGame(game){
+}
+
+const loader = {ori: loadGame("ori"), portal: loadGame("portal")}
--- games/nightmare-crew/code/add-scene.js
+++
@@ -1,7 +0,0 @@
-
-function addScene(THREE){
-    const renderer = new THREE.WebGLRenderer();
-    renderer.setSize( window.innerWidth, window.innerHeight );
-    document.body.appendChild( renderer.domElement );
-}
-export {addScene}
