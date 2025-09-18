32ee298c66fda29cc73fee682b3d38bbadf03387
oddalan <0ddalan300@gmail.com>
2025-09-18 12:16:04 -0600

    made more things. (:

---
+++ random-python/dumb-ai.py
@@ -0,0 +1,16 @@
+greeting = "Hi stupid!"
+print(greeting)
+
+word1_name = "good"
+word1_root = "none"
+word2_name = "ok"
+word2_root = "good"
+word3_name = "bad"
+word3_root = "none"
+word4_name = "not good"
+word4_root = "bad"
+
+you = input("how are you ")
+
+if you == "good" or you == "ok":
+    print("Thats too bad.")
--- random-python/python.py
+++ random-python/python.py
@@ -1,5 +1,8 @@
-from datetime import datetime
-from time import sleep
-date = datetime.now()
-print("The date is " + str(date.month) + "/" + str(date.day) + "/" + str(date.year) + " just so you know.")
-print("Are you having a good day?")
+# from datetime import datetime
+# from time import sleep
+# date = datetime.now()
+# print("The date is " + str(date.month) + "/" + str(date.day) + "/" + str(date.year) + " just so you know.")
+# print("Are you having a good day?")
+
+# print(str(b'F0 9F 98 82' , encoding="utf-8"))
+
