d09107e143fa7fc3c2f19e53659faa453e5dbdfc
oddalan <0ddalan300@gmail.com>
2025-10-12 19:16:35 -0600

    remove installed c libraries

--- games/mach-one/main.cpp
+++ games/mach-one/main.cpp
@@ -1,8 +1,10 @@
-#include <GL/glew.h>
-#include <GLFW/glfw3.h>
+#include <include/GL/glew.h>
+#include <include/GLFW/glfw3.h>
 #include <iostream>
 
 using namespace std;
+
+void init(GLFWwindow* window)
 
 void display(GLFWwindow* window, double currentTime){
     glClearColor(1.0, 0.0, 0.0, 1.0);
